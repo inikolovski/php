b59dcb4f540b0a19004fcaff9a9e7d267d817e88
Igor Nikolovski <i.nikolovski@gmail.com>
2023-11-04 14:39:13 +0100

    info.php

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?php echo "If you see this you went through BGP, VRRP,  eBPF, Cilium CNI, Kubernetes, LoadBalancer, Cilium Ingress,  nginx deployment, phpdeployment. And you got this code from Ceph CSI over CephFS originating from some github repo downloaded on pod restart to the pvc."; ?>
+<?php 
+phpinfo();
+//echo "If you see this you went through BGP, VRRP,  eBPF, Cilium CNI, Kubernetes, LoadBalancer, Cilium Ingress,  nginx deployment, phpdeployment. And you got this code from Ceph CSI over CephFS originating from some github repo downloaded on pod restart to the pvc."; 
+?>
