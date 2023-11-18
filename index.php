49d174c3660cfdb1fcd5ea95e47fd5c22f784b4e
Igor <i.nikolovski@gmail.com>
2023-11-18 18:48:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
-session_start();
-//phpinfo();
-var_dump($_SESSION);
+
+   session_start();
+
+   if( isset( $_SESSION['counter'] ) ) {
+
+      $_SESSION['counter'] += 1;
+
+   }else {
+
+      $_SESSION['counter'] = 1;
+
+   }
+
+   $my_Msg = "This page is visited ".  $_SESSION['counter'];
+
+   $my_Msg .= " time during this session.";
+
 ?>
+
+<html>
+
+   <head>
+
+      <title>Starting a PHP session</title>
+
+   </head>
+
+   <body>
+
+      <?php  echo ( $my_Msg ); ?>
+
+   </body>
+
+</html>
